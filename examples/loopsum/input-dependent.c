66e1409acae4661370305ff0c1caea11af6a86b8
yanxx297 <yanxx297@umn.edu>
2018-08-09 21:34:37 -0500

    Add another example to test loop summarization features

---
+++ examples/loopsum/input-dependent.c
@@ -0,0 +1,41 @@
+// An very simple program used as a introducing example Automatic Partial Loop 
+// Summarization in Dynamic Test Generation
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int func(int x){
+	int c = 0;
+	int p = 0;
+	while (1){
+		if (x <= 0)
+			break;
+		if (c == 50)
+			return 1; // error1
+		c = c + 1;
+		p = p + c;
+		x = x - 1;
+	}
+	if (c == 30)
+		return 2; // error 2
+	else
+		return 0;
+}
+
+int main(int argc, char **argv){
+	int input;
+	int res;
+	if (argc != 2){
+		fprintf(stderr, "Usage: input-dependent <integer>\n");
+		exit(-1);
+	}
+	input = atoi(argv[1]);
+	res = func(input);
+	if (res == 1)
+		printf("Error code 1.\n");
+	else if(res == 2)
+		printf("Error code 2.\n");
+	else
+		printf("End with normal result.\n");
+	return 0;
+}
